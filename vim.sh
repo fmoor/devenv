560c55be8b4805a496c071d045f23e66e4c70183
fmoor <fmoor@gmx.com>
2024-08-07 16:17:52 -0700

    update vim.sh

--- vim.sh
+++ vim.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+# asdf is required
+# dotfiles is required
+
 (
 
 cd
@@ -11,8 +14,12 @@ mkdir -p ~/.vim
 asdf plugin add nodejs || true
 asdf install nodejs 17.9.0 || true
 asdf global nodejs 17.9.0 || true
+npm install --global yarn
 ln -fs ~/src/dotfiles/vimrc ~/.vimrc
 ln -fs ~/src/dotfiles/vim-ftplugin ~/.vim/ftplugin
+
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt update
 sudo apt install -y vim-gtk
 # PlugInstall
 
